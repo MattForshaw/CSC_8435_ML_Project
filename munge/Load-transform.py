05f8ca604bf170d8f15566a925e45e9c65aee2ba
m-tyrrell <mtyrrell@ymail.com>
2019-01-19 14:02:05 +0000

    completed preprocessing, setup model to run on azure

--- munge/Load-transform.py
+++ munge/Load-transform.py
@@ -7,32 +7,28 @@ Created on Fri Jan 18 19:30:22 2019
 """
 
 %matplotlib inline
-import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import pandas as pd
 import os
 from glob import glob
-import seaborn as sns
 from PIL import Image
-np.random.seed(123)
+
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 from sklearn import preprocessing
 from sklearn.preprocessing import label_binarize
 from sklearn.metrics import confusion_matrix
 import itertools
 
-import keras
-from keras.utils.np_utils import to_categorical # used for converting labels to one-hot-encoding
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
-from keras import backend as K
-import itertools
-from keras.layers.normalization import BatchNormalization
-from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
-
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from imread import imread, imsave
+from keras.utils.np_utils import to_categorical
 
 
 # Assign project template data directory
@@ -56,29 +52,103 @@ images_df['image_id'] = images_df['path'].str[-16:-4]
 # Join image_df with meta on image id
 meta = pd.merge(meta, images_df, how='left', on=['image_id'])
 
-
-
-
-meta['image'] = meta['path'].map(lambda x: np.asarray(Image.open(images_df[0][x]).resize((100,75))))
-meta['foo'] = meta.apply(lambda x: np.asarray(Image.open(images_df['path'][x]).resize((100,75))), axis=1)
-
-# Create new empty column for image 
-meta['image'] = ""
+## Create new empty column for image 
+#meta['image'] = ""
 # Iterate through images df, downsampling to 100x75 pixels, converting to array and inserting into meta df
-for i in range(0,len(images_df)-1):
-    meta['image'][i] = np.asarray(Image.open(images_df['path'][i]).resize((100,75)))
-    
+#for i in range(0,len(images_df)-1):
+##    meta.image[i] = np.asarray(Image.open(meta.path[i]).resize((100,75)))
+#     meta.iloc[i,9] = np.asarray(Image.open(meta.iloc[i,7]).resize((100,75)))
+
+meta['image'] = meta['path'].map(lambda x: np.asarray(Image.open(x).resize((100,75))))
+meta.to_csv(os.path.join(data_dir,"meta_asarray.csv"), sep='\t')
+
+# Extract predictor variable (images) and labels as seperate vectors
+X=meta['image']
+Y=meta['dx']
+
+# Iterate through images vector and normalise image array (divide by max RGB value = 255)
+X = np.asarray(meta['image'].tolist())
+X = X.astype('float32')
+X /= 255
+
+# Split test/train set for predictor and label variables
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20,random_state=10)
+
+# Perform one-hot encoding on the labels
+
+# Integer encode each category
+label_encoder = LabelEncoder()
+Y_train_enc = label_encoder.fit_transform(Y_train)
+Y_test_enc = label_encoder.fit_transform(Y_test)
+# One-hot encode integers
+Y_train = to_categorical(Y_train_enc, num_classes = 7)
+Y_test = to_categorical(Y_test_enc, num_classes = 7)
+
+
+
+Y_train.shape
+
+X_train, X_validate, Y_train, Y_validate = train_test_split(X_train, Y_train, test_size = 0.1, random_state = 10) 
+
+# Reshape image in 3 dimensions (height = 75px, width = 100px , canal = 3)
+x_train = X_train.reshape(X_train.shape[0], *(75, 100, 3))
+x_test = x_test.reshape(x_test.shape[0], *(75, 100, 3))
+X_validate = X_validate.reshape(X_validate.shape[0], *(75, 100, 3))
+
+
+X_train.shape
+x_train.shape
+
+
+X_validate.shape
 
 
 
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+# Verify array/image pipleline integrity by converting back to image
+plt.imshow(y)
+
+# invert encoding
+inverted = argmax(encoded[0])
+print(inverted)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
---
+++ src/CNN_01.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 23:14:53 2019
+
+@author: MT
+"""
+
+
+
+
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras import backend as K
+import itertools
+from keras.layers.normalization import BatchNormalization
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+
+# Set random seed to ensure reproducability
+np.random.seed(10)      
+
+
+
+# Set the CNN model 
+# my CNN architechture is In -> [[Conv2D->relu]*2 -> MaxPool2D -> Dropout]*2 -> Flatten -> Dense -> Dropout -> Out
+input_shape = (75, 100, 3)
+num_classes = 7
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',padding = 'Same',input_shape=input_shape))
+model.add(Conv2D(32,kernel_size=(3, 3), activation='relu',padding = 'Same',))
+model.add(MaxPool2D(pool_size = (2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
+model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.40))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.summary()
+
+
+# Define the optimizer
+optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+
+# Compile the model
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+# Set a learning rate annealer
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+
+# With data augmentation to prevent overfitting 
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+datagen.fit(X_train)
+
+
+# Fit the model
+epochs = 50 
+batch_size = 10
+history = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_validate,y_validate),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
+
+
+
+# Save model
+model.save(filepath)
+
+loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
+loss_v, accuracy_v = model.evaluate(x_validate, y_validate, verbose=1)
+print("Validation: accuracy = %f  ;  loss_v = %f" % (accuracy_v, loss_v))
+print("Test: accuracy = %f  ;  loss = %f" % (accuracy, loss))
+model.save("model.h5")
+
+
+
+
+# Function to plot confusion matrix    
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Predict the values from the validation dataset
+Y_pred = model.predict(x_validate)
+# Convert predictions classes to one hot vectors 
+Y_pred_classes = np.argmax(Y_pred,axis = 1) 
+# Convert validation observations to one hot vectors
+Y_true = np.argmax(y_validate,axis = 1) 
+# compute the confusion matrix
+confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
+
+ 
+
+# plot the confusion matrix
+plot_confusion_matrix(confusion_mtx, classes = range(7)) 
